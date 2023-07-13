455b6de95fb478dbf906da07c365bf8646fae9c1
alireza171819 <alirezakhalili1380.12.11@gmail.com>
2023-07-13 19:28:00 +0330

    add api

---
+++ Plugins/Nop.Plugin.Api/Configuration/ApiConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Nop.Core.Configuration;
+using System;
+
+namespace Nop.Plugin.Api.Configuration
+{
+    public class ApiConfiguration : IConfig, IDisposable
+    {
+        public int AllowedClockSkewInMinutes { get; set; } = 5;
+
+        public string SecurityKey { get; set; } = "NowIsTheTimeForAllGoodMenToComeToTheAideOfTheirCountry";
+
+        public void Dispose()
+        {
+        }
+    }
+}
